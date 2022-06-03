a3ebeb59be8e61e59c7efca038617f752258ce5d
otarampinelli <otaviorampinellielias@gmail.com>
2022-06-03 11:59:18 -0300

    feat: created some rules to verify if the guess is right

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     // ! === macro
     println!("Guess the Number!");
-    println!("Write your tip!.");
 
-    let mut palpite = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    loop {
+        println!("Write your guess.");
 
-    io::stdin().read_line(&mut palpite).expect("Failed to read input!");
+        let mut guess = String::new();
 
-    println!("You said: {}", palpite);
+        io::stdin().read_line(&mut guess).expect("Failed to read input! \n");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You said: {} \n", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Very low guess! \n"),
+            Ordering::Greater => println!("Very high guess! \n"),
+            Ordering::Equal => {
+                println!("You got the guess right! \n");
+                break;
+            }
+        }
+    }
 }
